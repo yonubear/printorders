3c4c92c23cbfec5fcb2c7629cbd62200ca69bbab
yonubear <yonubear@neondawg.com>
2025-02-19 20:02:42 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,58 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
-    entry: path.join(__dirname, 'src', 'index.js'),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'js'),
-        publicPath: '/apps/printorders/js/',
-        filename: 'printorders.js',
-        chunkFilename: 'chunks/[name].js'
+      path: path.resolve(__dirname, 'js'),
+      filename: 'printorders.js',
+      chunkFilename: 'chunks/[name].[contenthash].js',
+      clean: true
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            ['@babel/preset-react', { runtime: 'automatic' }]
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1
-                        }
-                    },
-                    'postcss-loader'
-                ]
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react']
             }
-        ]
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'postcss-loader'
+          ]
+        }
+      ]
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
-        alias: {
-            '@': path.resolve(__dirname, 'src')
+      extensions: ['.js', '.jsx']
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all'
+          }
         }
-    }
+      }
+    },
+    plugins: [
+      isProduction && process.env.ANALYZE && new BundleAnalyzerPlugin()
+    ].filter(Boolean),
+    devtool: isProduction ? 'source-map' : 'eval-source-map'
+  };
 };
