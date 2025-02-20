ad9284733c11ae3d1211f6f1ca4f2cb7a671d68d
yonubear <yonubear@neondawg.com>
2025-02-19 20:08:37 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,95 @@
 const path = require('path');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
 
   return {
     mode: isProduction ? 'production' : 'development',
-    entry: './src/index.js',
+    entry: {
+      main: './src/index.js',
+      // Add additional entry points for code splitting
+      admin: './src/admin.js',
+      vendor: ['react', 'react-dom', 'lucide-react']
+    },
     output: {
       path: path.resolve(__dirname, 'js'),
-      filename: 'printorders.js',
-      chunkFilename: 'chunks/[name].[contenthash].js',
-      clean: true
+      filename: isProduction ? '[name].[contenthash].js' : '[name].js',
+      chunkFilename: isProduction ? 'chunks/[name].[contenthash].js' : 'chunks/[name].js',
+      clean: true,
+      publicPath: '/apps/printorders/js/'
+    },
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            parse: {
+              ecma: 8,
+            },
+            compress: {
+              ecma: 5,
+              warnings: false,
+              comparisons: false,
+              inline: 2,
+              drop_console: isProduction,
+            },
+            mangle: {
+              safari10: true,
+            },
+            output: {
+              ecma: 5,
+              comments: false,
+              ascii_only: true,
+            },
+          },
+          parallel: true,
+        }),
+        new CssMinimizerPlugin(),
+        new ImageMinimizerPlugin({
+          minimizer: {
+            implementation: ImageMinimizerPlugin.imageminMinify,
+            options: {
+              plugins: [
+                ['gifsicle', { interlaced: true }],
+                ['jpegtran', { progressive: true }],
+                ['optipng', { optimizationLevel: 5 }],
+                ['svgo', { plugins: [{ name: 'preset-default' }] }],
+              ],
+            },
+          },
+        }),
+      ],
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+              return `vendor.${packageName.replace('@', '')}`;
+            },
+          },
+          common: {
+            name: 'common',
+            minChunks: 2,
+            priority: -10
+          }
+        },
+      },
+      runtimeChunk: 'single',
     },
     module: {
       rules: [
@@ -21,6 +99,8 @@ module.exports = (env, argv) => {
           use: {
             loader: 'babel-loader',
             options: {
+              cacheDirectory: true,
+              cacheCompression: false,
               presets: ['@babel/preset-react']
             }
           }
@@ -29,30 +109,73 @@ module.exports = (env, argv) => {
           test: /\.css$/,
           use: [
             'style-loader',
-            'css-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                modules: {
+                  auto: true,
+                  localIdentName: isProduction
+                    ? '[hash:base64]'
+                    : '[path][name]__[local]'
+                }
+              }
+            },
             'postcss-loader'
           ]
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/i,
+          type: 'asset',
+          parser: {
+            dataUrlCondition: {
+              maxSize: 8 * 1024 // 8kb
+            }
+          }
         }
       ]
     },
-    resolve: {
-      extensions: ['.js', '.jsx']
-    },
-    optimization: {
-      splitChunks: {
-        chunks: 'all',
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all'
-          }
-        }
-      }
-    },
     plugins: [
-      isProduction && process.env.ANALYZE && new BundleAnalyzerPlugin()
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(
+          isProduction ? 'production' : 'development'
+        ),
+      }),
+      new webpack.ProgressPlugin(),
+      new DuplicatePackageCheckerPlugin(),
+      new WebpackManifestPlugin({
+        fileName: 'asset-manifest.json',
+      }),
+      isProduction && new CompressionPlugin({
+        algorithm: 'gzip',
+        test: /\.(js|css|html|svg)$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+      isProduction && new WorkboxPlugin.GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true,
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      }),
+      process.env.ANALYZE && new BundleAnalyzerPlugin()
     ].filter(Boolean),
-    devtool: isProduction ? 'source-map' : 'eval-source-map'
+    devtool: isProduction ? 'source-map' : 'eval-source-map',
+    performance: {
+      hints: isProduction ? 'warning' : false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000
+    },
+    stats: {
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false
+    },
+    cache: {
+      type: 'filesystem',
+      buildDependencies: {
+        config: [__filename]
+      }
+    }
   };
 };
