55fb24dc4b1b4515d5ba251590df079166036383
yonubear <yonubear@neondawg.com>
2025-02-24 18:41:20 -0500

    1.1.0

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,9 +1,9 @@
 <?php
 declare(strict_types=1);
 
-// Register the app
-\OCP\App::register([
-    'order' => 10,
-    'id' => 'printorders',
-    'name' => 'Print Orders'
-]);
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use OCP\AppFramework\App;
+use OCA\PrintOrders\AppInfo\Application;
+
+$app = new Application();
